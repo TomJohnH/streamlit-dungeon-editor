8ffbd33b5e23f8e3fb2c6fb5f10751bf8cfa6aea
TomJohnH <thasiow@onet.pl>
2023-03-15 17:42:09 +0100

    visual refactor

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,16 @@
-# https://streamlit-feature-demos-data-editor-foundationdemo-bhdzga.streamlit.app/
-
 import streamlit as st
 import pandas as pd
 
+# wide layout
 st.set_page_config(layout="wide")
+
+#################################
+#
+#
+#           Functions
+#
+#
+##################################
 
 
 @st.cache_data
@@ -12,57 +19,9 @@ def fetch_data(level_name):
     return df
 
 
-# edited_df = st.experimental_data_editor(df)
-
-st.subheader("The Dungeon level editor")
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    st.session_state.df = pd.read_csv(uploaded_file)
-if uploaded_file is None:
-    if "df" not in st.session_state:
-        st.session_state.df = fetch_data("level1.csv")
-
-
-# ----------------------------------------------------
-
-
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
-
-local_css("style.css")
-
-tileset = {
-    "@": "https://oshi.at/ZMUu/avRY.gif",
-    "W": "https://thumbs2.imgbox.com/10/db/7zaxbIP8_t.png",  # wall
-    "FP": "https://thumbs2.imgbox.com/29/22/5rTLr6WH_t.png",  # floor_plain
-    # "FP": "https://oshi.at/PQkn/ExtR.png",  # floor 1 tilset 2
-    "CAT": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/cat.gif",  # cat
-    "M": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/monster.gif",  # monster, skeleton
-    "FS": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_stain_1.png",
-    "E": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
-    "FE3": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_edge_3.png",  # floor_edge_3
-    "WON": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_n.png",  # Wall_outer_n
-    "WOE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_e.png",  # Wall_outer_e
-    "WONE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_ne.png",  # Wall_outer_ne
-    "WOW": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_w.png",  # Wall_outer_w
-    "WONW": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_nw.png",  # wall_outer_nw
-    "WFR": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_front_right.png",  # wall front right
-    "WTR": "https://oshi.at/QpWg/Mfxv.png",  # wall top right
-    # "DK": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/wall_missing_brick_2.png",  # darkness
-    "WMB": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/wall_missing_brick_2.png",  # wall missing brick 1
-    "BOX": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/box.png",  # box
-    "DR": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/darkness_right.png",  # darkenss right
-    "DB": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/darkness_bottom.png",  # darkness bottom
-    "T": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/torch.gif",  # torch
-    "FMN1": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_n_1.png",  # floor_mud_n_1
-    "FMN2": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_n_2.png",  # floor_mud_n_2
-    "FMNE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_ne.png",  # floor_mud_ne
-    "CGOF": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/chest_golden_open_full.png",  # chest_golden_open_full
-    "CGOO": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/chest_open_empty.png",  # chest_open_empty
-    "FL": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Floor_ladder.png",  # Floor_ladder
-}
 
 
 def path_to_image_html(path):
@@ -106,11 +65,82 @@ def level_renderer(df, game_objects):
     return level_html
 
 
+#################################
+#
+#
+#           Constants
+#
+#
+##################################
+
+
+tileset = {
+    "@": "https://oshi.at/ZMUu/avRY.gif",
+    "W": "https://thumbs2.imgbox.com/10/db/7zaxbIP8_t.png",  # wall
+    "FP": "https://thumbs2.imgbox.com/29/22/5rTLr6WH_t.png",  # floor_plain
+    # "FP": "https://oshi.at/PQkn/ExtR.png",  # floor 1 tilset 2
+    "CAT": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/cat.gif",  # cat
+    "M": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/monster.gif",  # monster, skeleton
+    "FS": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_stain_1.png",
+    "E": "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==",
+    "FE3": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_edge_3.png",  # floor_edge_3
+    "WON": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_n.png",  # Wall_outer_n
+    "WOE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_e.png",  # Wall_outer_e
+    "WONE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_ne.png",  # Wall_outer_ne
+    "WOW": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_w.png",  # Wall_outer_w
+    "WONW": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_outer_nw.png",  # wall_outer_nw
+    "WFR": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Wall_front_right.png",  # wall front right
+    "WTR": "https://oshi.at/QpWg/Mfxv.png",  # wall top right
+    # "DK": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/wall_missing_brick_2.png",  # darkness
+    "WMB": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/wall_missing_brick_2.png",  # wall missing brick 1
+    "BOX": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/box.png",  # box
+    "DR": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/darkness_right.png",  # darkenss right
+    "DB": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/darkness_bottom.png",  # darkness bottom
+    "T": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/other/torch.gif",  # torch
+    "FMN1": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_n_1.png",  # floor_mud_n_1
+    "FMN2": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_n_2.png",  # floor_mud_n_2
+    "FMNE": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/floor_mud_ne.png",  # floor_mud_ne
+    "CGOF": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/chest_golden_open_full.png",  # chest_golden_open_full
+    "CGOO": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/chest_open_empty.png",  # chest_open_empty
+    "FL": "https://raw.githubusercontent.com/TomJohnH/streamlit-dungeon/main/graphics/tileset/Floor_ladder.png",  # Floor_ladder
+}
+
+
+#################################
+#
+#
+#            The App
+#
+#
+##################################
+
+# css style
+
+local_css("style.css")
+
+# title
+st.subheader("The Dungeon level editor")
+
+# file upload for ones wanting to upload their own file
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    st.session_state.df = pd.read_csv(uploaded_file)
+if uploaded_file is None:
+    if "df" not in st.session_state:
+        st.session_state.df = fetch_data("level1.csv")
+
+
+# ------------------ SPLIT TO TABS--------------------------
+
+
 tab1, tab2 = st.tabs(["Editor", "Tilset"])
+
+# main editor
 
 with tab1:
 
     col1, col2 = st.columns(2)
+
     with col2:
         level_data = st.experimental_data_editor(
             st.session_state.df, use_container_width=True, height=620
@@ -132,10 +162,11 @@ with tab1:
                 regex="[a-zA-Z0-9]+", value="E"
             )
 
+# tileset
+
 with tab2:
 
     df_tileset = pd.DataFrame.from_dict(tileset, orient="index", columns=["look"])
-    # st.dataframe(df_tileset)
 
     st.markdown(
         df_tileset.to_html(escape=False, formatters=dict(look=path_to_image_html)),
